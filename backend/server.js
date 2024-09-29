3be093b0155860515c124cbe56dfcc9b1384556e
Payal Sharma <payalsharmareal@gmail.com>
2024-09-29 22:47:16 +0530

    changes

--- backend/server.js
+++ backend/server.js
@@ -7,9 +7,24 @@ const app = express();
 const PORT  = process.env.PORT || 5000;
 
 //Middleware
-app.use(cors());
+const corsOpption = {
+    origin: 'http://localhost:3000', //frontend URL
+    optionsSuccessstatus : 200,
+}
+app.use(cors(corsOpption));
+//Alternatively, Use Vite's Proxy (Preferred for Development)
 app.use(bodyparser.json());
 app.use(express.json());
+
+//before API routes
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, '../frontend/dist')));
+
+//Handle SPA routing, return index.html for any unknown routes
+app.get('*', (req, res)=>{
+    res.sendFile(path.join(__dirname, '../frontend/dist', 'index.html'));
+})
 
 //Routes
 // const apiroute = require('./routes/api');
