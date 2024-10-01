0602d7cdc040c6c9737bf6b3916f515f416e189f
Payal Sharma <payalsharmareal@gmail.com>
2024-10-01 23:09:58 +0530

    tried out integration

--- backend/server.js
+++ backend/server.js
@@ -21,9 +21,20 @@ const path = require('path');
 
 app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
+// // Serve static files from the 'public' directory
+// app.use(express.static(path.join(__dirname, '../frontend/public')));
+
+// // Set up views
+// app.set('view engine', 'ejs'); // Install ejs if you want to use templating
+// app.set('views', path.join(__dirname, '../frontend/views'));
+
 //Handle SPA routing, return index.html for any unknown routes
-app.get('*', (req, res)=>{
-    res.sendFile(path.join(__dirname, '../frontend/dist', 'index.html'));
+// app.get('*', (req, res)=>{
+//     res.sendFile(path.join(__dirname, '../frontend/dist', 'index.html'));
+// })
+
+app.get("/", (req, res)=>{
+    res.send('server running')
 })
 
 //Routes
@@ -33,4 +44,5 @@ app.get('*', (req, res)=>{
 //Start server
 app.listen(PORT, ()=>{
     console.log(`Server is running on port ${PORT}`);
+    console.log(`http://localhost:${PORT}`);
 });
